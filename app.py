7bd39b07a8f3aa68009e7993a4639cc5f90e0859
FabPaul <paulfopa0@gmail.com>
2023-10-24 12:54:52 +0100

    Still in the making

---
+++ app.py
@@ -0,0 +1,54 @@
+import json
+from flask import Flask, render_template, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Load the pharmacies from the JSON file
+with open("pharmacies.json", "r") as f:
+    pharmacies_data = json.load(f)
+
+# Define the routes for the application
+@app.route("/")
+def index():
+    current_time = datetime.now().strftime("%H:%M:%S")
+    return render_template('index.html', current_time=current_time)
+
+@app.route("/pharmacies")
+def display_pharmacies():
+    return render_template("pharmacies.html", pharmacies=pharmacies_data["pharmacies"])
+
+@app.route("/pharmacy_details/<int:pharmacy_id>")
+def pharmacy_details(pharmacy_id):
+    pharmacy = pharmacies_data["pharmacies"][pharmacy_id - 1]
+    return render_template("pharmacy.html", pharmacy=pharmacy, pharmacy_id=pharmacy_id)
+
+@app.route("/medications/<int:pharmacy_id>")
+def medications(pharmacy_id):
+    pharmacy = pharmacies_data["pharmacies"][pharmacy_id - 1]
+    return render_template("medications.html", pharmacy=pharmacy, pharmacy_id=pharmacy_id)
+
+@app.route("/location/<int:pharmacy_id>")
+def location(pharmacy_id):
+    pharmacy = pharmacies_data["pharmacies"][pharmacy_id - 1]
+    return render_template("location.html", pharmacy=pharmacy, pharmacy_id=pharmacy_id)
+
+@app.route("/contact/<int:pharmacy_id>")
+def contact(pharmacy_id):
+    pharmacy = pharmacies_data["pharmacies"][pharmacy_id - 1]
+    return render_template("contact.html", pharmacy=pharmacy, pharmacy_id=pharmacy_id)
+
+@app.route("/search", methods=["GET"])
+def search_medications():
+    query = request.args.get("query")
+    search_results = []
+    if query:
+        for pharmacy in pharmacies_data["pharmacies"]:
+            for medication in pharmacy["medications"]:
+                if query.lower() in medication["name"].lower():
+                    search_results.append({"pharmacy": pharmacy["name"], "medication": medication})
+    return render_template("search.html", query_results=search_results)
+
+# Start the Flask app
+if __name__ == "__main__":
+    app.run(debug=True)
